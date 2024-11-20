a1c6c5ff438a9757db8b35114907e44757d95ca6
6182201036 <6182201036@student.unpar.ac.id>
2024-11-20 10:55:05 +0700

    Penambahan Server Side untuk Sidang TA

---
+++ SidangTA/src/main/java/org/example/SidangTA/TubesSidangTaApplication.java
@@ -0,0 +1,13 @@
+package org.example.SidangTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesSidangTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesSidangTaApplication.class, args);
+	}
+
+}
---
+++ SidangTA/src/test/java/org/example/SidangTA/TubesSidangTaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.SidangTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesSidangTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
