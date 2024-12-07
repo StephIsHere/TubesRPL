cff7dfab03b46523e94cd087ac8acc9c8b2b6c11
6182201036 <6182201036@student.unpar.ac.id>
2024-12-07 23:47:16 +0700

    Penambahan fitur overlay untuk penambahan dan penghapusan user, dan membuat user yang tidak aktif terlihat

--- src/main/resources/static/js/admin/tambahPeserta.js
+++ src/main/resources/static/js/admin/tambahPeserta.js
@@ -1,30 +1,55 @@
-function generate(event){
+function generate(event) {
     let nama = document.getElementById("nama").value.toLowerCase();
     let email = document.getElementById("email");
     let password = document.getElementById("password");
     let peran = document.getElementById("role");
     let npm = document.getElementById("npm");
 
-    email.value = nama+"@unpar";
+    email.value = nama + "@unpar";
 
-    if (peran.value == "admin"){
+    if (peran.value == "admin") {
         password.value = "admin";
         npm.value = "-";
         npm.disabled = true;
-    }
-    else if (peran.value == "koord"){
+    } else if (peran.value == "koord") {
         password.value = "koord";
         npm.value = "-";
         npm.disabled = true;
-    }
-    else if (peran.value == "dosen"){
+    } else if (peran.value == "dosen") {
         password.value = "dosen";
         npm.value = "-";
         npm.disabled = true;
-    }
-    else if (peran.value == "mahasiswa"){
+    } else if (peran.value == "mahasiswa") {
         password.value = nama;
         npm.value = "";
         npm.disabled = false;
     }
+}
+
+function showSuccessOverlay() {
+    let overlay = document.createElement("div");
+    overlay.id = "successOverlay";
+    overlay.className = "overlay";
+
+    overlay.innerHTML = `
+            <div class="messageBox">
+                <p>User successfully added!</p>
+                <button onclick="confirmSuccess()">OK</button>
+            </div>
+        `;
+    document.body.appendChild(overlay);
+}
+
+function confirmSuccess() {
+    const overlay = document.getElementById('successOverlay');
+    if (overlay) {
+        overlay.remove();
+    }
+    const form = document.getElementById('addUserForm');
+    form.submit();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    showSuccessOverlay();
 }
