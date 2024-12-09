4e0ddd7ed184b4a9b8c7170c9e0cff58bd090b1d
OLIVIA <i22006@ftis>
2024-12-09 15:06:00 +0700

    form AddSidang

--- src/main/resources/static/js/koordinator/addSidang.js
+++ src/main/resources/static/js/koordinator/addSidang.js
@@ -23,7 +23,7 @@ function ambilNamaMahasiswa() {
 }
 
 function ambilNamaPembimbingUtama() {
-    let nik = document.getElementById("nik").value;
+    let nik = document.getElementById("nikPembimbingUtama").value;
 
     if (nik.length > 0) {
         fetch(`/home/addSidang/getUserByNik?nik=${nik}`)
@@ -32,16 +32,88 @@ function ambilNamaPembimbingUtama() {
                 console.log(data);
                 console.log(data.nama);
                 if (data.nama) {
-                    document.getElementById("namaMahasiswa").value = data.nama;
+                    document.getElementById("namaPembimbingUtama").value = data.nama;
                 } else {
-                    document.getElementById("namaMahasiswa").value = "Nama tidak ditemukan";
+                    document.getElementById("namaPembimbingUtama").value = "Nama tidak ditemukan";
                 }
             })
             .catch(error => {
                 console.error('Error:', error);
-                document.getElementById("mahasiswa").value = "Error fetching data";
+                document.getElementById("namaPembimbingUtama").value = "Error fetching data";
             });
     } else {
-        document.getElementById("mahasiswa").value = "";
+        document.getElementById("namaPembimbingUtama").value = "";
+    }
+}
+
+function ambilNamaPembimbingPendamping() {
+    let nik = document.getElementById("nikPembimbingPendamping").value;
+
+    if (nik.length > 0) {
+        fetch(`/home/addSidang/getUserByNik?nik=${nik}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                console.log(data.nama);
+                if (data.nama) {
+                    document.getElementById("namaPembimbingPendamping").value = data.nama;
+                } else {
+                    document.getElementById("namaPembimbingPendamping").value = "Nama tidak ditemukan";
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById("namaPembimbingPendamping").value = "Error fetching data";
+            });
+    } else {
+        document.getElementById("namaPembimbingPendamping").value = "";
+    }
+}
+
+function ambilNamaKetuaPenguji() {
+    let nik = document.getElementById("nikKetuaPenguji").value;
+
+    if (nik.length > 0) {
+        fetch(`/home/addSidang/getUserByNik?nik=${nik}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                console.log(data.nama);
+                if (data.nama) {
+                    document.getElementById("namaKetuaPenguji").value = data.nama;
+                } else {
+                    document.getElementById("namaKetuaPenguji").value = "Nama tidak ditemukan";
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById("namaKetuaPenguji").value = "Error fetching data";
+            });
+    } else {
+        document.getElementById("namaKetuaPenguji").value = "";
+    }
+}
+
+function ambilNamaAnggotaPenguji() {
+    let nik = document.getElementById("nikAnggotaPenguji").value;
+
+    if (nik.length > 0) {
+        fetch(`/home/addSidang/getUserByNik?nik=${nik}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                console.log(data.nama);
+                if (data.nama) {
+                    document.getElementById("namaAnggotaPenguji").value = data.nama;
+                } else {
+                    document.getElementById("namaAnggotaPenguji").value = "Nama tidak ditemukan";
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                document.getElementById("namaAnggotaPenguji").value = "Error fetching data";
+            });
+    } else {
+        document.getElementById("namaAnggotaPenguji").value = "";
     }
 }
