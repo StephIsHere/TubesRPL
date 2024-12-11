e347643530288fb2f430806828bc56426296c130
6182201078 <6182201078@student.unpar.ac.id>
2024-12-11 19:16:18 +0700

    fixed sql query

--- src/test/java/com/example/TubesRPL/TubesRplApplicationTests.java
+++ src/test/java/com/example/TubesRPL/TubesRplApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.TubesRPL;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TubesRplApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
