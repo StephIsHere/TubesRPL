0bbdae66df105f9b7f109f1594c054cec5f3ead4
6182201078 <6182201078@student.unpar.ac.id>
2024-12-15 23:08:00 +0700

    add penilaian

--- src/main/java/com/example/TubesRPL/sidang/SidangRepoJdbc.java
+++ src/main/java/com/example/TubesRPL/sidang/SidangRepoJdbc.java
@@ -9,10 +9,15 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.RowMapper;
 
+import com.example.TubesRPL.komponenNilai.*;
+
 @Repository
 public class SidangRepoJdbc implements SidangRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private KomponenNilaiRepository kompNilaiRepo;
 
     @Override
     public List<Sidang> findSidang(int idSidang) {
@@ -185,6 +190,84 @@ public class SidangRepoJdbc implements SidangRepository {
         String sql = "UPDATE sidang SET catatan = ? WHERE judul = ?"; 
         int rowsAffected = jdbcTemplate.update(sql, catatan, sidang.getJudul()); 
         return rowsAffected > 0; 
-    }  
+    } 
+
+    @Override
+    public void saveNilaiPenguji1(int idSidang, int ttl, int km, int pt, int p, int pm) {
+        String sql = "INSERT INTO komponenNilaiSidang (idSidang, idKomponen, nilai) "+    "VALUES (?, 5, ?), "+
+        "(?, 6, ?), "+
+        "(?, 7, ?), "+
+        "(?, 8, ?), "+
+        "(?, 9, ?); ";
+        jdbcTemplate.update(sql, idSidang, ttl, idSidang, km, idSidang, pt, idSidang, p, idSidang, pm);
+    }
+
+    @Override
+    public void saveNilaiPenguji2(int idSidang, int ttl, int km, int pt, int p, int pm) {
+        String sql = "INSERT INTO komponenNilaiSidang (idSidang, idKomponen, nilai) "+    "VALUES (?, 14, ?), "+
+        "(?, 15, ?), "+
+        "(?, 16, ?), "+
+        "(?, 17, ?), "+
+        "(?, 18, ?); ";
+        jdbcTemplate.update(sql, idSidang, ttl, idSidang, km, idSidang, pt, idSidang, p, idSidang, pm);
+    }
+
+    @Override
+    public void saveNilaiPembimbing(int idSidang, int ttl, int km, int pb, int pm) {
+        String sql = "INSERT INTO komponenNilaiSidang (idSidang, idKomponen, nilai) "+    "VALUES (?, 10, ?), "+
+        "(?, 11, ?), "+
+        "(?, 12, ?), "+
+        "(?, 13, ?); ";
+        jdbcTemplate.update(sql, idSidang, ttl, idSidang, km, idSidang, pb, idSidang, pm);
+    }
+
+    @Override
+    public boolean checkNilaiPenguji1(int idSidang) {
+        String sql = "SELECT COUNT(*) FROM komponenNilaiSidang WHERE idKomponen = 5 AND idSidang = ?";
+        int count = jdbcTemplate.queryForObject(sql, new Object[]{idSidang}, Integer.class);
+        return count > 0;
+    }
+
+    @Override
+    public boolean checkNilaiPenguji2(int idSidang) {
+        String sql = "SELECT COUNT(*) FROM komponenNilaiSidang WHERE idKomponen = 14 AND idSidang = ?";
+        int count = jdbcTemplate.queryForObject(sql, new Object[]{idSidang}, Integer.class);
+        return count > 0;
+    }
+
+    @Override
+    public boolean checkNilaiPembimbing(int idSidang) {
+        String sql = "SELECT COUNT(*) FROM komponenNilaiSidang WHERE idKomponen = 10 AND idSidang = ?";
+        int count = jdbcTemplate.queryForObject(sql, new Object[]{idSidang}, Integer.class);
+        return count > 0;
+    }
+
+    @Override
+    public void saveNilaiMain(int idSidang) {
+        boolean nPeng1 = checkNilaiPenguji1(idSidang);
+        boolean nPeng2 = checkNilaiPenguji2(idSidang);
+        boolean nPem = checkNilaiPembimbing(idSidang);
+        if (nPeng1 && nPeng2 && nPem) {
+            String sql = "INSERT INTO komponenNilaiSidang (idSidang, idKomponen, nilai) "+  "VALUES (?, 1, ?), "+
+            "(?, 2, ?), "+
+            "(?, 3, ?), "+
+            "(?, 4, ?); ";
+
+            double nilaiPeng1 = getNilaiPerKomponen(idSidang, 5)*kompNilaiRepo.getBobot(5) + getNilaiPerKomponen(idSidang, 6)*kompNilaiRepo.getBobot(6) + getNilaiPerKomponen(idSidang, 7)*kompNilaiRepo.getBobot(7) + getNilaiPerKomponen(idSidang, 8)*kompNilaiRepo.getBobot(8) + getNilaiPerKomponen(idSidang, 9)*kompNilaiRepo.getBobot(9);
+
+            double nilaiPeng2 = getNilaiPerKomponen(idSidang, 14)*kompNilaiRepo.getBobot(14) + getNilaiPerKomponen(idSidang, 15)*kompNilaiRepo.getBobot(15) + getNilaiPerKomponen(idSidang, 16)*kompNilaiRepo.getBobot(16) + getNilaiPerKomponen(idSidang, 17)*kompNilaiRepo.getBobot(17) + getNilaiPerKomponen(idSidang, 18)*kompNilaiRepo.getBobot(18);
+
+            double nilaiPem = getNilaiPerKomponen(idSidang, 10)*kompNilaiRepo.getBobot(10) + getNilaiPerKomponen(idSidang, 11)*kompNilaiRepo.getBobot(11) + getNilaiPerKomponen(idSidang, 12)*kompNilaiRepo.getBobot(12) + getNilaiPerKomponen(idSidang, 13)*kompNilaiRepo.getBobot(13);
+
+            double nilaiKoord = 100.0;
+
+            jdbcTemplate.update(sql, idSidang, nilaiPeng1, idSidang, nilaiPeng2, idSidang, nilaiPem, idSidang, nilaiKoord);
+        }
+    }
+
+    public int getNilaiPerKomponen (int idSidang, int idKomponen) {
+        String sql = "SELECT nilai FROM komponenNilaiSidang WHERE idKomponen = ? AND idSidang = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{idKomponen, idSidang}, Integer.class);
+    }
 
 }
